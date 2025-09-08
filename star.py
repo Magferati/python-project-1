04d50f955e7cf535e95591fe8c7defea2fafeedc
zannatul magferati <magferatizannatul@gmail.com>
2025-09-08 22:15:57 +0600

    Add files via upload

---
+++ star.py
@@ -0,0 +1,18 @@
+import turtle
+turtle.bgcolor("black")
+
+t=turtle.Turtle()
+t.pencolor ("cyan")
+t.speed(1)
+t.rt(144)
+t.fd(200)
+t.rt(144)
+t.fd(200)
+t.rt(144)
+t.fd(200)
+t.rt(144)
+t.fd(200)
+t.rt(144)
+t.fd(200)
+
+
